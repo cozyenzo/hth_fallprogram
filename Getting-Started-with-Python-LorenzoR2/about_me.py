b94b39a2521f58c2162673d4235b98bd58e4b699
cozyenzo <lorenzodottreyes@gmail.com>
2022-10-06 18:38:47 -0700

    completing first python activity files

---
+++ Getting-Started-with-Python-LorenzoR2/about_me.py
@@ -0,0 +1,20 @@
+Name = 'Lorenzo Reyes'
+Pref = 'Lorenzo'
+Age = 17
+has_used_python = True
+Hobbies = ['Sewing', 'Cooking', 'Playing guitar']
+
+fav_things = {
+"movie": "Grave of the Fireflies",
+"food": "Udon",
+"hobbie": Hobbies[0]
+"anime": "FMAB"
+}
+
+print(Name)
+print(Pref)
+print(Age)
+print(Hobbies)
+
+
+all_vars = dict(vars())
